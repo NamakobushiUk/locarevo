432389d208b1fa34f8c5a44f92f7032fd183d8c2
NamakobushiUk <178769581+NamakobushiUk@users.noreply.github.com>
2025-04-15 22:33:38 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/locarevo/', // ✅ ここをリポジトリ名に合わせて変更！
+
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
